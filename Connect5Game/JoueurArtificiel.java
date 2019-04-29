bac51ce68af073d2252cea8906c78e1e0e48b92b
Carlos Cortez-Linares <cortez-linares.carlos@courrier.uqam.ca>
2019-04-29 17:45:50 -0400

    fonction eval template

--- Connect5Game/JoueurArtificiel.java
+++ Connect5Game/JoueurArtificiel.java
@@ -22,84 +22,69 @@ public class JoueurArtificiel implements Joueur {
 
     @Override
     public Position getProchainCoup(Grille grille, int delais) {
+
         ArrayList<Integer> casesvides = new ArrayList<Integer>();
-        ArrayList<Integer> pionsJ1 = new ArrayList<Integer>();
-        ArrayList<Integer> pionsJ2 = new ArrayList<Integer>();
         int nbcol = grille.getData()[0].length;
-        for(int l=0;l<grille.getData().length;l++){
+
+        for(int l=0;l<grille.getData().length;l++) {
+
             for(int c=0;c<nbcol;c++) {
-                if(grille.getData()[l][c]==0){
+
+                if(grille.getData()[l][c]==0)
                     casesvides.add(l*nbcol+c);
-                } else if (grille.getData()[l][c]==1){
-                    pionsJ1.add(l*nbcol+c);
-                } else if (grille.getData()[l][c]==2){
-                    pionsJ2.add(l*nbcol+c);
-                }
             }
+
         }
-        eval(Grille);
 
         int choix = random.nextInt(casesvides.size());
         choix = casesvides.get(choix);
         return new Position(choix / nbcol, choix % nbcol);
+    }
 
-        private int getJoueurCourant(ArrayList j1, ArrayList j2){
-            if(j1.size() > j2.size()){
-                return 1;
-            } else if(j1.size() == j2.size()){
-                return 0;
+
+    public int evalMeilleurCoup(Grille grille ) {
+
+        int result  = 0 ;
+
+        // Iteration horizontal
+        for (int l = 0; l < grille.data.length; l++) {
+            for (int c = 0; c < grille.data[0].length; c++) {
+
+                // logique ici
+            }
+
+        }
+
+        // Iteration vertical
+        for (int c = 0; c < grille.data[0].length; c++) {
+            for (int l = 0; l < grille.data.length; l++) {
+
+                // logique ici
+
+            }
+
+        }
+
+
+        // iteration diagonal \\\\\ de gauche a droite
+        for (int l = 4; l < grille.getData().length; l++) {
+            for (int c = 4; c < grille.getData()[l].length; c++) {
+
+                // logique ici
+
+            }
+        }
+
+        // iteration diagonal ///// de droite a gauche
+        for (int l = grille.getData().length - 5; l >= 0; l--) {
+            for (int c = 4; c < grille.getData()[l].length; c++) {
+
+                // logique ici
+
             }
         }
 
 
-        private int eval(Grille grille){
-        int pointsJ1 = 0;
-        // Parcours des colonnes
-        for(int l=0;l<grille.getData().length;l++){
-            for(int c=0;c<grille.getData()[0].length;c++){
-             if(grille.getData()[l][c]==0){
-                if (grille.geData()[l][c+1] == 0 && grille.getData()[l][c+2] == 0 && grille.getData()[l][c+3] == 0 {
-                    pointsJ1 = pointsJ1 + 1000;
-                    c=c+3;
-                }else if(grille.getData()[l][c+1]==0 && grille.getData()[l][c+2]==0){
-                  pointsJ1 = pointsJ1 + 100;
-                  c=c+2;
-                }else if(grille.getData()[l][c+1]==0){
-                 pointsJ1 = pointsJ1 + 10;
-                 c = c+1;
-                }else{
-                    pointsJ1= pointsJ1 + 1 ;
-                }      
-             }   
-            }
-        }       
-        // Parcours des lignes
-    for(int c=0;c<grille.getData()[0].length;c++){
-          for(int l=0;l<grille.getData().length;l++){
-             if(grille.getData()[l][c]==0){
-                if (grille.geData()[l+1][c] == 0 && grille.getData()[l+2][c] == 0 && grille.getData()[l+3][c] == 0 {
-                    pointsJ1 = pointsJ1 + 1000;
-                    c=c+3;
-                }else if(grille.getData()[l+1][c]==0 && grille.getData()[l+2][c]==0){
-                  pointsJ1 = pointsJ1 + 100;
-                  c=c+2;
-                }else if(grille.getData()[l+1][c]==0){
-                 pointsJ1 = pointsJ1 + 10;
-                 c = c+1;
-                }else{
-                    pointsJ1= pointsJ1 + 1 ;
-                }
-            }
-          }
-        }             
-        
-        // Parcours des diagonales  
-            //todo       
-               
-       }
-
+        return result ;
     }
-   
-
-
 }
